2dc31f5fc37a3dcf64096c543a252df74e3d8137
Paul Moya <apmoya@lksnext.com>
2024-10-31 08:28:18 +0100

    capacitor con almacenamento interno de la aplicacion

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'capacitor.xml.app',
+  appName: 'capacitor-xml-app',
+  webDir: 'dist/capacitor-xml-app',
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { DeviceContextService } from './services/devicecontext.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>{{ title }}</h1>
+    <p>{{ deviceMessage }}</p>
+    <div *ngIf="infoTerminal && !infoTerminal.error">
+      <p>PDA: {{ infoTerminal.PDA }}</p>
+      <p>Gestor: {{ infoTerminal.Gestor }}</p>
+      <p>Centro: {{ infoTerminal.CENTRO }}</p>
+      <p>SIG: {{ infoTerminal.SIG }}</p>
+    </div>
+    <p *ngIf="infoTerminal?.error">{{ infoTerminal.error }}</p>
+  `,
+  styles: []
 })
-export class AppComponent {
-  title = 'capacitor-xml-app';
+export class AppComponent implements OnInit {
+  title = 'Capacitor XML PoolApp';
+  deviceMessage: string = '';
+  infoTerminal: any;
+
+  constructor(
+    private deviceContext: DeviceContextService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.deviceContext.loadConfigFile().then(() => {
+      this.deviceMessage = `Iniciando desde: ${this.deviceContext.deviceType}`;
+    });
+
+    this.deviceContext.infoTerminal$.subscribe(data => {
+      this.infoTerminal = data;
+      this.cdr.detectChanges();
+    });
+  }
 }
